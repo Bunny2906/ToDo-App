b75b2e54596f4fc12df13d3b02286bdb0cc63da8
Varun Kumar Reddy Pothula <117117037+Bunny2906@users.noreply.github.com>
2023-05-24 12:16:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ let editNote = (thisEle)=>{
         var x = window.matchMedia("(max-width: 480px)");
         if(x.matches)
         {
-            editableInput.style.width="60%";
+            editableInput.style.width="50%";
         }
         thisEle.parentElement.replaceChild(editableInput,thisEle.previousElementSibling);
     }
